fe276fa88d2f4bdc11a8f096323def2c0fcf1415
TabAh007 <tabriz007@gmail.com>
2022-04-19 12:58:44 +0400

    updated colors

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x7BDD9fc61209828fdf40e09953452e7C7b6Aa726"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Emperor Mikado NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The Emperor Mikado NFT Project is a first project of our team, and people who shows support will be whitelisted for our new NFT project will be launching on September.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
